5032ab51ab25988fd724cb929ff586cb83ca39ac
Klaas de Boer <kladebo@xs4all.nl>
2016-04-20 20:25:27 +0200

    added Promise polyfill so IE works also

--- work/js/app.js
+++ work/js/app.js
@@ -15,12 +15,12 @@
         paths: {
             main: ['app/main.min', 'app/main'],
             domReady: ['//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min', 'lib/domReady.min'],
-            //promise: ['//cdnjs.cloudflare.com/ajax/libs/es6-promise/3.2.1/es6-promise.min', 'lib/es6-promise.min'],
+            promise: ['//cdnjs.cloudflare.com/ajax/libs/es6-promise/3.2.1/es6-promise.min', 'lib/es6-promise.min'],
             epoch: ['lib/epoch_classes']
         }
     });
 
     // Start loading the main app file. Put all of
     // your application logic in there.
-    requirejs(['main', 'domReady', 'epoch']);
+    requirejs(['main', 'domReady', 'promise', 'epoch']);
 }());
